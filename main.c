a15ad891c7f346974a3e3acc3d553127040849c4
JuanSapriza <juan.sapriza@bqn.com.uy>
2022-04-10 10:18:24 -0300

    Limpiando código. Commita para recuperar un pedazo que se borró por error

--- main.c
+++ main.c
@@ -14,124 +14,43 @@
 
 #define NON_CONNECTABLE_ADV_INTERVAL    MSEC_TO_UNITS(100, UNIT_0_625_MS)  /**< The advertising interval for non-connectable advertisement (100 ms). This value can vary between 100ms to 10.24s). */
 
-#define APP_BEACON_INFO_LENGTH          0x17                               /**< Total length of information advertised by the Beacon. */
-#define APP_ADV_DATA_LENGTH             0x15                               /**< Length of manufacturer specific data in the advertisement. */
-#define APP_DEVICE_TYPE                 0x02                               /**< 0x02 refers to Beacon. */
-#define APP_MEASURED_RSSI               0x01                               /**< The Beacon's measured RSSI at 1 meter distance in dBm. */
-#define APP_COMPANY_IDENTIFIER          0x0203                             /**< Company identifier for Nordic Semiconductor ASA. as per www.bluetooth.org. */
-#define APP_MAJOR_VALUE                 0x04, 0x04                         /**< Major value used to identify Beacons. */
-#define APP_MINOR_VALUE                 0x06, 0x06                         /**< Minor value used to identify Beacons. */
-#define APP_BEACON_UUID                 0x05, 0x05, 0x05, 0x05, \
-                                        0x05, 0x05, 0x05, 0x05, \
-                                        0x05, 0x05, 0x05, 0x05, \
-                                        0x05, 0x05, 0x05, 0x05             /**< Proprietary UUID for Beacon. */
-//******
-#define APP_DEVICE_NAME "Caravana 125"
-//******
+#define APP_AD_LEN_FLAG     0X02
+#define APP_AD_TYPE_FLAG    0X01
+#define APP_AD_DATA_FLAG    0X06    // Nonconnectable, undirected 
+#define APP_AD_LEN_MSD      0X03
+#define APP_AD_TYPE_MSD     0XFF
+#define APP_AD_DATA_MSD     0X0203  // Nordic Semiconductors
+#define APP_AD_LEN_SHLN     0X0F    // Depende de la informacion que pongamos!!
+#define APP_AD_TYPE_SHLN    0x08    // Shortened Local Name   
+#define APP_AD_DATA_SHLN    0x31,0x32,0x33,0x34,0x35,0x36,0x37,0x38,0x39,0x41,0x42,0X43,0X44,0x45  // ID on the Tag
 
-#define DEAD_BEEF                       0xDEADBEEF                         /**< Value used as error code on stack dump, can be used to identify stack location on stack unwind. */
-
-#if defined(USE_UICR_FOR_MAJ_MIN_VALUES)
-#define MAJ_VAL_OFFSET_IN_BEACON_INFO   18                                 /**< Position of the MSB of the Major Value in m_beacon_info array. */
-#define UICR_ADDRESS                    0x10001080                         /**< Address of the UICR register used by this example. The major and minor versions to be encoded into the advertising data will be picked up from this location. */
-#endif
-
-
-//******
-static uint8_t              m_enc_srdata[BLE_GAP_ADV_SET_DATA_SIZE_MAX];  /**< Buffer for storing an encoded scan set. */
-//******
-
-
-
+static uint8_t advData[] = {
+   APP_AD_LEN_FLAG,     
+   APP_AD_TYPE_FLAG,    
+   APP_AD_DATA_FLAG,       
+   APP_AD_TYPE_MSD,     
+   APP_AD_DATA_MSD,     
+   APP_AD_LEN_SHLN,      
+   APP_AD_TYPE_SHLN,    
+   APP_AD_DATA_SHLN 
+};
 
 static ble_gap_adv_params_t m_adv_params;                                  /**< Parameters to be passed to the stack when starting advertising. */
 static uint8_t              m_adv_handle = BLE_GAP_ADV_SET_HANDLE_NOT_SET; /**< Advertising handle used to identify an advertising set. */
-static uint8_t              m_enc_advdata[BLE_GAP_ADV_SET_DATA_SIZE_MAX];  /**< Buffer for storing an encoded advertising set. */
 
-/**@brief Struct that contains pointers to the encoded advertising data. */
-static ble_gap_adv_data_t m_adv_data =
-{
+static ble_gap_adv_data_t m_adv_data ={
     .adv_data =
     {
-        .p_data = m_enc_advdata,
-        .len    = BLE_GAP_ADV_SET_DATA_SIZE_MAX
+        .p_data = advData,
+        .len    = sizeof(advData)
     },
-    .scan_rsp_data =
-    {
-//******
-        .p_data = m_enc_srdata,
-        .len    = BLE_GAP_ADV_SET_DATA_SIZE_MAX
-//******
-    }
 };
 
 
-static uint8_t m_beacon_info[APP_BEACON_INFO_LENGTH] =                    /**< Information advertised by the Beacon. */
-{
-    APP_DEVICE_TYPE,     // Manufacturer specific information. Specifies the device type in this
-                         // implementation.
-    APP_ADV_DATA_LENGTH, // Manufacturer specific information. Specifies the length of the
-                         // manufacturer specific data in this implementation.
-    APP_BEACON_UUID,     // 128 bit UUID value.
-    APP_MAJOR_VALUE,     // Major arbitrary value that can be used to distinguish between Beacons.
-    APP_MINOR_VALUE,     // Minor arbitrary value that can be used to distinguish between Beacons.
-    APP_MEASURED_RSSI    // Manufacturer specific information. The Beacon's measured TX power in
-                         // this implementation.
-};
-
-
-static uint8_t arrayDePrueba[] = {0x02,0x01,0x06,0x03,0xFF,0x02,0X03,0X0F,0X08,0x31,0x32,0x33,0x34,0x35,0x36,0x37,0x38,0x39,0x41,0x42,0X43,0X44,0x45};
-
-
-/**@brief Function for initializing the Advertising functionality.
- *
- * @details Encodes the required advertising data and passes it to the stack.
- *          Also builds a structure to be passed to the stack when starting advertising.
- */
-static void advertising_init(void)
-{
+static void advertising_init(void){
     uint32_t      err_code;
-    ble_advdata_t advdata;
 
-//******
-    ble_advdata_t srdata; // This line was added !!!
-//******
-    uint8_t       flags = BLE_GAP_ADV_FLAG_BR_EDR_NOT_SUPPORTED;
-
-    ble_advdata_manuf_data_t manuf_specific_data;
-
-    manuf_specific_data.company_identifier = APP_COMPANY_IDENTIFIER;
-    manuf_specific_data.data.p_data = (uint8_t *) m_beacon_info;
-    manuf_specific_data.data.size   = APP_BEACON_INFO_LENGTH;
-
-    // Build and set advertising data.
-    memset(&advdata, 0, sizeof(advdata));
-
-    advdata.name_type             = BLE_ADVDATA_NO_NAME;
-    advdata.flags                 = flags;
-    advdata.p_manuf_specific_data = &manuf_specific_data;
-
-
-
-    //err_code = ble_advdata_encode(&advdata, m_adv_data.adv_data.p_data, &m_adv_data.adv_data.len);
-    //APP_ERROR_CHECK(err_code);
-
-//****
-
-    //m_adv_data.adv_data.p_data[0]=02; //undirected unconnectable adv
-    //m_adv_data.adv_data.p_data[1]=01;
-    //m_adv_data.adv_data.p_data[2]=06;
-
-    //m_adv_data.adv_data.p_data[2]=06;
-
-    memcpy(m_adv_data.adv_data.p_data,arrayDePrueba,sizeof(arrayDePrueba));
-    m_adv_data.adv_data.len = sizeof(arrayDePrueba);
-
-//*****
-
-    // Initialize advertising parameters (used when starting advertising).
     memset(&m_adv_params, 0, sizeof(m_adv_params));
-
     m_adv_params.properties.type = BLE_GAP_ADV_TYPE_NONCONNECTABLE_NONSCANNABLE_UNDIRECTED;
     m_adv_params.p_peer_addr     = NULL;    // Undirected advertisement.
     m_adv_params.filter_policy   = BLE_GAP_ADV_FP_ANY;
@@ -141,20 +60,10 @@ static void advertising_init(void)
     err_code = sd_ble_gap_adv_set_configure(&m_adv_handle, &m_adv_data, &m_adv_params);
     //APP_ERROR_CHECK(err_code);
 
- //******  
-    // Build and set scan response data.
-    memset(&srdata, 0, sizeof(srdata));
-    srdata.name_type             = BLE_ADVDATA_FULL_NAME;
-    //err_code = ble_advdata_encode(&srdata, m_adv_data.scan_rsp_data.p_data, &m_adv_data.scan_rsp_data.len);
-    //APP_ERROR_CHECK(err_code);
- //******   
 }
 
 
-/**@brief Function for starting advertising.
- */
-static void advertising_start(void)
-{
+static void advertising_start(void){
     ret_code_t err_code;
 
     err_code = sd_ble_gap_adv_start(m_adv_handle, APP_BLE_CONN_CFG_TAG);
@@ -165,12 +74,7 @@ static void advertising_start(void)
 }
 
 
-/**@brief Function for initializing the BLE stack.
- *
- * @details Initializes the SoftDevice and the BLE event interrupt.
- */
-static void ble_stack_init(void)
-{
+static void ble_stack_init(void){
     ret_code_t err_code;
 
     err_code = nrf_sdh_enable_request();
@@ -187,78 +91,38 @@ static void ble_stack_init(void)
     APP_ERROR_CHECK(err_code);
 }
 
-
-/**@brief Function for initializing LEDs. */
-static void leds_init(void)
-{
+static void leds_init(void){
     ret_code_t err_code = bsp_init(BSP_INIT_LEDS, NULL);
     APP_ERROR_CHECK(err_code);
 }
 
+static void timers_init(void){
 
-/**@brief Function for initializing timers. */
-static void timers_init(void)
-{
-    ret_code_t err_code = app_timer_init();
-    APP_ERROR_CHECK(err_code);
 }
 
-
-/**@brief Function for initializing power management.
- */
 static void power_management_init(void)
-{
-    ret_code_t err_code;
-    err_code = nrf_pwr_mgmt_init();
-    APP_ERROR_CHECK(err_code);
+
+
 }
 
-
-static void idle_state_handle(void)
-{
+static void idle_state_handle(void){
      nrf_pwr_mgmt_run();
 }
 
 
-/**
- * @brief Function for application main entry.
- */
-int main(void)
-{
-//******
-    ret_code_t err_code;
-    ble_gap_conn_sec_mode_t sec_mode;
-//******
-    // Initialize.
+int main(void){
     timers_init();
     leds_init();
     power_management_init();
     ble_stack_init();
-
-    
-//******    
-    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&sec_mode);
-    err_code = sd_ble_gap_device_name_set(&sec_mode,(const uint8_t*) APP_DEVICE_NAME, strlen(APP_DEVICE_NAME));
-    APP_ERROR_CHECK(err_code);
-//******
-    
-    
-    
     advertising_init();
 
-    // Start execution.
+
     advertising_start();
-//******
+
     bsp_board_led_on(3);
-//******
-    // Enter main loop.
-    for (;; )
-    {
+    for (;; ){
         idle_state_handle();
     }
 }
 
-
-/**
- * @}
- */
