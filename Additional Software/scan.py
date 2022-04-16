eb8819bab5c331176164e0488ec5005bd22c6dc7
JuanSapriza <juan.sapriza@bqn.com.uy>
2022-04-15 22:26:47 -0300

    El script funciona mostrando en cada iteración el ID obtenido

--- Additional Software/scan.py
+++ Additional Software/scan.py
@@ -4,9 +4,17 @@ ble = BLERadio()
 print("scanning")
 found = set()
 scan_responses = set()
-for advertisement in ble.start_scan():
-    if advertisement.complete_name:
-        if advertisement.complete_name not in found:
-            found.add(advertisement.complete_name)
-            print("ID: "+advertisement.complete_name+" | "+str(advertisement.rssi)+"dBi")
-            print(found)
+i = 0
+while True:
+    for advertisement in ble.start_scan(interval=0.05):
+        if advertisement.complete_name: 
+            if advertisement.complete_name not in found:
+                found.add(advertisement.complete_name)
+                print(str(i)+" ID: "+advertisement.complete_name+" | "+str(advertisement.rssi)+"dBi")
+                ble.stop_scan()
+                i = i+1
+            else:
+                ble.stop_scan()
+                i = i+1
+                
+            
